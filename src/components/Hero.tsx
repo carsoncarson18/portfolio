f1e2a10b6b3f06ceabf7b5fbf71348ad1799c447
Carson <carsondavie18@gmail.com>
2025-07-22 01:06:35 -0400

    new background video

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -18,7 +18,7 @@ export default function Hero() {
                 playsInline
                 className="absolute inset-0 w-full h-full object-cover z-0"
             >
-                <source src="/bgVisual2.mp4" type="video/mp4" />
+                <source src="/bgVisual1.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
 
